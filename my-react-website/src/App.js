b9664c79c464ec40e25f2c4b28334c56e1b92978
rag2504 <ragraichura12@gmail.com>
2025-04-23 20:54:08 +0530

    first commit

--- my-react-website/src/App.js
+++ my-react-website/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import './components/Header.css'; // Component-specific styles
 import './styles/styles.css'; // Import your CSS file
 
 const App = () => {
